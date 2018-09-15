304aa96f7904d681014ebb97ac57b8aab69d4843
Ronga Paul <rnp@n-az30103.local>
2018-09-15 17:28:57 +0200

    move “use this...” to separate file

---
+++ js/appendix.js
@@ -0,0 +1,52 @@
+// THIS IS EXECUTED WHEN EVERYTHING IS LOADED
+// var everythingLoaded = setInterval(function() {
+//   if (/loaded|complete/.test(document.readyState)) {
+//     clearInterval(everythingLoaded);
+//   }
+// }, 10);
+
+// USE THIS IF SOMETHING CANT LOAD CORRECTLY
+// $(window).load(function(){
+// });
+
+// THIS IS DONE AT EVERY SCROLL
+// $(window).scroll(function (event) {
+//    });
+
+  // SCROLL TO AN ELEMENT FUNCTION
+  //$('html, body').animate({
+  //    scrollTop:$('.element').offset().top
+  //}, 1000);
+
+  // SCROLL TO A CALCULATED HEIGHT
+//$('html, body').animate({
+  //      scrollTop:$(scrollHeight).offset().top
+  //}, 1000);
+
+  // SIMPLE CLICK
+//    $(".menu").click(function(){
+// });
+
+  // SET A DELAY IN A FUNCTION
+// setTimeout(function()
+// {
+// }, 250);
+
+// KEEP A DIV SQUARE HACK
+// var largeur = $('.un-quart').width();
+// $('.un-quart').height(largeur);
+
+//   	$(window).resize(function(){
+//   		var largeur = $('.un-quart').width();
+//   		$('.un-quart').height(largeur);
+//    });
+
+  // THIS IS EXECUTED ON RESIZE
+  // $(window).resize(function(){
+
+  // });
+
+  // THIS IS EXECUTED DEPENDING ON WINDOW WIDTH
+// if($(window).width() > 500)
+// {
+// }
